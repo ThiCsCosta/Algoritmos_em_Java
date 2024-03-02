e6ad987581cf2eb7f13f23cf5e006da53a85558a
ThiCsCosta <thycscosta@gmail.com>
2024-03-01 21:59:35 -0300

    Aquivo NumeroParOuImpar.java criado

--- src/resolucao/NumeroParOuImpar.java
+++ src/resolucao/NumeroParOuImpar.java
@@ -3,10 +3,18 @@ package resolucao;
 import java.util.Scanner;
 
 public class NumeroParOuImpar {
-
+	
 	public static void main(String[] args) {
-		
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite um número: ");
+		int numero = sc.nextInt();
+		if(numero % 2 == 0) {
+			System.out.println("O número digitado é Par!");
+		}
+		else {
+			System.out.println("O número digitado é Ímpar.");
+		}
+		sc.close();
 	}
 
 }
